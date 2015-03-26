d5be3fea1d586493bd1e74427451288aec6d823d
Kai Kang <kai@Kais-MBP.home>
2015-03-26 03:13:44 -0400

    Search function

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,41 @@
 angular.module("homeApp", ["ngMaterial", "ngRoute"])
     .controller("AppCtrl", function($scope){
-        console.log("Hi");
+        $scope.query = "";
+        $scope.todos = [
+            {
+                face : '/images/60.jpeg',
+                title: 'Guitar Strum',
+                who: 'Hongyu Li',
+                when: '3:08PM',
+                notes: "A tutorial on how to strum the guitar"
+            },
+            {
+                face : '/images/60.jpeg',
+                title: 'Flip The Pan',
+                who: 'Kai Kang',
+                when: '3:08PM',
+                notes: " Flip the pan while stir-frying to add more flavor to your dishes"
+            },
+            {
+                face : '/images/60.jpeg',
+                title: 'Brunch this weekend?',
+                who: 'Min Li Chan',
+                when: '3:08PM',
+                notes: " I'll be in your neighborhood doing errands"
+            },
+            {
+                face : '/images/60.jpeg',
+                title: 'Brunch this weekend?',
+                who: 'Min Li Chan',
+                when: '3:08PM',
+                notes: " I'll be in your neighborhood doing errands"
+            },
+            {
+                face : '/images/60.jpeg',
+                title: 'Brunch this weekend?',
+                who: 'Min Li Chan',
+                when: '3:08PM',
+                notes: " I'll be in your neighborhood doing errands"
+            },
+        ];
     });
